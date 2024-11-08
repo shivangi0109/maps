395e3a5ae35eb10c26fc4b24f7e6642672a8db89
Shivangi Srivastava <shi06dec@gmail.com>
2024-11-08 16:34:14 -0700

    Explore google maps type definition files & add env file for google maps api key

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
 /// <reference types="@types/google.maps" />
+import dotenv from "dotenv";
+dotenv.config();
 
-import { User } from './User';
-import { Company } from './Company';
+// Get the API key from the environment
+const apiKey = process.env.API_KEY;
 
-const user = new User();
-console.log(user);
+if (apiKey) {
+  const script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+  script.async = true;
+  script.defer = true;
 
-const company = new Company();
-console.log(company);
+  // Append the script to the document head
+  document.head.appendChild(script);
+} else {
+  console.error("API Key is missing!");
+}
 
-google
+// Callback function for Google Maps
+(window as any).initMap = function () {
+  // Initialize your map or other code here
+
+  new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 1,
+    center: {
+      lat: 0,
+      lng: 0
+    }
+  });
+};
+
+// import { User } from './User';
+// import { Company } from './Company';
+
+// const user = new User();
+// console.log(user);
+
+// const company = new Company();
+// console.log(company);
+
