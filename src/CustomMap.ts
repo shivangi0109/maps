a0eefb11fa7dc9fc2cfdb11eeb82d80eb90cd9b3
Shivangi Srivastava <shi06dec@gmail.com>
2024-11-12 17:10:34 -0700

    Restricting Access with Interfaces

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,13 @@
-import { User } from './USer';
+import { User } from './User';
 import { Company } from './Company';
+
+// Instructions to every other class on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,7 +22,7 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: User | Company) : void {
+  addMarker(mappable: Mappable) : void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
